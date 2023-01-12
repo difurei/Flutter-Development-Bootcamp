b71d7ad9dffea3643ce3dadd14bf4e236b2afa20
Xatsker <youkixiro@gmail.com>
2023-01-12 23:27:19 +0300

    finishing UI

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,13 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
-          primaryColor: Colors.lightBlue,
-          scaffoldBackgroundColor: Colors.white),
+          primaryColor: Colors.amber,
+          scaffoldBackgroundColor: Colors.grey[800],
+        textTheme: TextTheme(button: TextStyle(color: Colors.black)),
+        appBarTheme: AppBarTheme(color: Colors.transparent)
+      ),
       home: PriceScreen(),
     );
   }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -1,4 +1,7 @@
+import 'package:bitcoin_ticker/coin_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:io' show Platform;
 
 class PriceScreen extends StatefulWidget {
   @override
@@ -6,11 +9,48 @@ class PriceScreen extends StatefulWidget {
 }
 
 class _PriceScreenState extends State<PriceScreen> {
+  String selectedCurrency = currenciesList[0];
+
+  DropdownButton<String> androidDropdownMenu() {
+    List<DropdownMenuItem<String>> dropdownMenuItemList = [];
+
+    for (String item in currenciesList) {
+      dropdownMenuItemList
+          .add(DropdownMenuItem(child: Text(item), value: item));
+    }
+
+    return DropdownButton<String>(
+        value: selectedCurrency,
+        items: dropdownMenuItemList,
+        onChanged: (value) {
+          setState(() {
+            selectedCurrency = value;
+          });
+        });
+  }
+
+  CupertinoPicker IOSPicker() {
+    List<Text> pickerItems = [];
+
+    for (String item in currenciesList) {
+      pickerItems.add(Text(item));
+    }
+
+    return CupertinoPicker(
+      itemExtent: 32.0,
+      onSelectedItemChanged: (selectedIndex) {
+        print(selectedIndex);
+      },
+      children: pickerItems,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('🤑 Coin Ticker'),
+        centerTitle: true,
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -19,7 +59,7 @@ class _PriceScreenState extends State<PriceScreen> {
           Padding(
             padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
             child: Card(
-              color: Colors.lightBlueAccent,
+              color: Colors.amber,
               elevation: 5.0,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10.0),
@@ -30,9 +70,8 @@ class _PriceScreenState extends State<PriceScreen> {
                   '1 BTC = ? USD',
                   textAlign: TextAlign.center,
                   style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
-                  ),
+                      fontSize: 20.0,
+                      color: Theme.of(context).textTheme.button.color),
                 ),
               ),
             ),
@@ -41,8 +80,8 @@ class _PriceScreenState extends State<PriceScreen> {
             height: 150.0,
             alignment: Alignment.center,
             padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: null,
+            color: Colors.amber,
+            child: Platform.isIOS ? IOSPicker() : androidDropdownMenu(),
           ),
         ],
       ),
